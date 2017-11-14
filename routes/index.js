9313305ea47413257e2bc0847023a9a3489b4aaf
nhu-quan.dang <nhu-quan.dang@eptica.com>
2017-11-14 16:17:26 +0100

    add milligramCss

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bitter coffee and the rain' });
 });
 
 module.exports = router;
